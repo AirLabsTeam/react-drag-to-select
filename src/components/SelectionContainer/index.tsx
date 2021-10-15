e9cd70fe9018c8fc4784882d052c4f29c56731d8
Dan Wilt <dwilt4rville@gmail.com>
2021-10-15 14:58:07 -0400

    removed state from MouseSelection

--- src/components/SelectionContainer/index.tsx
+++ src/components/SelectionContainer/index.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useImperativeHandle, useRef, useState } from 'react';
+import React, { forwardRef, useImperativeHandle, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { MouseSelectionRef, SelectionBox } from '../../utils/types';
 
@@ -9,41 +9,52 @@ export interface MouseSelectionProps extends React.HTMLAttributes<HTMLDivElement
  */
 const MouseSelection = forwardRef(({ style = {}, ...props }: MouseSelectionProps, ref) => {
   const containerRef = useRef<HTMLDivElement>(null);
-  const [selectionBox, setSelectionBox] = useState<null | SelectionBox>(null);
+  const selectionBoxRef = useRef<HTMLDivElement>(null);
 
   useImperativeHandle(
     ref,
     (): MouseSelectionRef => ({
       getBoundingClientRect: () => containerRef.current?.getBoundingClientRect(),
       getParentBoundingClientRect: () => containerRef?.current?.parentElement?.getBoundingClientRect(),
-      drawSelectionBox: setSelectionBox,
-      clearSelectionBox: () => setSelectionBox(null),
+      drawSelectionBox: (box: SelectionBox) => {
+        if (selectionBoxRef.current) {
+          const styles: React.CSSProperties = {
+            border: '1px solid #4C85D8',
+            background: 'rgba(155, 193, 239, 0.4)',
+            position: 'absolute',
+            pointerEvents: 'none',
+            ...style,
+            top: box.top,
+            left: box.left,
+            width: box.width,
+            height: box.height,
+          };
+
+          selectionBoxRef.current.style.cssText = `
+            
+          `;
+        }
+      },
+      clearSelectionBox: () => {
+        if (selectionBoxRef.current) {
+          selectionBoxRef.current.style.cssText = `
+            top: 0;
+            left: 0;
+            width: 0;
+            height: 0;
+          `;
+        }
+      },
     }),
   );
 
   return (
     <div ref={containerRef}>
-      {!!selectionBox &&
-        !!containerRef.current &&
-        ReactDOM.createPortal(
-          <div
-            {...props}
-            style={{
-              border: '1px solid #4C85D8',
-              background: 'rgba(155, 193, 239, 0.4)',
-              position: `absolute`,
-              zIndex: 99,
-              pointerEvents: 'none',
-              ...style,
-              ...(selectionBox || {}),
-            }}
-          />,
-          containerRef.current,
-        )}
+      {typeof document !== 'undefined'
+        ? ReactDOM.createPortal(<div ref={selectionBoxRef} {...props} />, containerRef.current || document.body)
+        : null}
     </div>
   );
 });
-
-MouseSelection.displayName = 'MouseSelection';
 
 export default MouseSelection;
