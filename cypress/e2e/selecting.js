55229064f3b1d7c6301408bfdb2d47111085a907
Dan Wilt <dwilt4rville@gmail.com>
2022-07-26 02:12:47 -0400

    Update selecting.js

--- cypress/e2e/selecting.js
+++ cypress/e2e/selecting.js
@@ -1,22 +1,7 @@
 /// <reference types="cypress" />
 
-// Welcome to Cypress!
-//
-// This spec file contains a variety of sample tests
-// for a todo list app that are designed to demonstrate
-// the power of writing tests in Cypress.
-//
-// To learn more about how Cypress works and
-// what makes it such an awesome testing tool,
-// please read our getting started guide:
-// https://on.cypress.io/introduction-to-cypress
-
-describe('example to-do app', () => {
+describe('react-drag-to-select example', () => {
   beforeEach(() => {
-    // Cypress starts out with a blank slate for each test
-    // so we must tell it to visit our website with the `cy.visit()` command.
-    // Since we want to visit the same URL at the start of all our tests,
-    // we include it in our beforeEach function so that it runs before each test
     cy.visit('/');
   });
 
